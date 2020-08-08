4a004b7244040749abfd3e56194241a523c71810
codershona <falgunimst95@gmail.com>
2020-08-08 14:16:22 +0200

    name create successfully working

--- routes/authors.js
+++ routes/authors.js
@@ -26,8 +26,25 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
 
+	const author = new Author({
+
+		name: req.body.name
+
+	})
+	author.save((err, newAuthor) => {
+		if (err) {
+			res.render('authors/new', {
+				author: author,
+				errorMessage: 'Error Creating Author'
+			})
+		} else {
+			// res.redirect(`authors/${newAuthor.id}`)
+			res.redirect(`authors`)
+		}
+	})
+
 	// res.send('Create')
-	res.send(req.body.name)
+	// res.send(req.body.name)
 
 })
 
